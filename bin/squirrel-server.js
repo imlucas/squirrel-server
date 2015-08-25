9612a67c4e6da436dea1b95ccf55d58efe02158e
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-08-25 18:10:35 -0400

    debugging deis

--- bin/squirrel-server.js
+++ bin/squirrel-server.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 var path = require('path');
 
-require(path.resolve(__dirname, '../')).listen(require('nconf').get('PORT'));
+require(path.resolve(__dirname, '../')).listen(require('nconf').get('PORT'), '0.0.0.0');
